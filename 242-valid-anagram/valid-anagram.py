691c9b19918f64dd2db514bcd60a7f9d4485b8e5
Zafer Eqbal <105378462+zafereqbal@users.noreply.github.com>
2026-08-21 21:02:01 +0530

    Time: 15 ms (86.39%) | Memory: 12.3 MB (98.69%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for char in t:
+            if char not in count:
+                return False
+
+            count[char] -= 1
+
+            if count[char] < 0:
+                return False
+
+        return True
