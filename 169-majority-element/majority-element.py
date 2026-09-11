4652198d7655162178859ef9bb38435584c2ef1d
Zafer Eqbal <105378462+zafereqbal@users.noreply.github.com>
2026-09-11 20:30:07 +0530

    Time: 15 ms (27.85%) | Memory: 13.6 MB (74.35%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
